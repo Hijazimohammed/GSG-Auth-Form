418087e954368a55663645e0c478d72cfcc041fa
Hijazimohammed <30778440+Hijazimohammed@users.noreply.github.com>
2023-07-07 09:44:09 +0300

    init foldres

---
+++ src/Pages/SignUp/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import styles from './style.module.css';
+
+export default class SignUp extends Component {
+  render() {
+    return <div>SignUp</div>;
+  }
+}
